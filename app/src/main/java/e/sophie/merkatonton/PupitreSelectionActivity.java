9efec671adbdcd0f7d70995a0b676254894c3527
Jonathan Malique <jonathan.malique@orange.fr>
2018-11-09 08:58:32 +0100

    Pages questions

--- app/src/main/java/e/sophie/merkatonton/PupitreSelectionActivity.java
+++ app/src/main/java/e/sophie/merkatonton/PupitreSelectionActivity.java
@@ -1,7 +1,9 @@
 package e.sophie.merkatonton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PupitreSelectionActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class PupitreSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pupitre_selection);
+    }
+
+    public void questions_poulpe (View view) {
+        startActivity(new Intent(this, QuestionPoulpeActivity.class));
     }
 }
